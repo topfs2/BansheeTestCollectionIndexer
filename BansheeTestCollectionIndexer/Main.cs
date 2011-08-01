f7119468b72ce68d60db07d3345cfaa4939af0e2
Tobias Arrskog <topfs2@xbmc.org>
2011-08-01 15:12:30 +0200

    Added initial solution

---
+++ BansheeTestCollectionIndexer/Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BansheeTestCollectionIndexer
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
+
